525e7c8c2a76c39ab314fe881ca678c00c8dbee1
36115152016.3 <36115152016.3@36DE194518.senacrjedu.local>
2017-11-16 11:59:08 -0200

    primeiro Commit

---
+++ src/olamundo/HelloWord.java
@@ -0,0 +1,10 @@
+package olamundo;
+
+import javax.swing.JOptionPane;
+
+public class HelloWord {
+	public static void main(String[] args) {
+	//	System.out.println("Olá Mundo java 2016.3");
+		JOptionPane.showMessageDialog(null, "Olá Mundo!");
+	}
+}
