386b8c28dd9cf100d7c1c6522565f0407568ddac
36115152016.3 <36115152016.3@36DE194518.senacrjedu.local>
2017-11-21 11:54:20 -0200

    aula 02

--- src/olamundo/HelloWord.java
+++ src/olamundo/HelloWord.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class HelloWord {
 	public static void main(String[] args) {
-	//	System.out.println("Olá Mundo java 2016.3");
-		JOptionPane.showMessageDialog(null, "Olá Mundo!");
+	System.out.println("Olá Mundo java 2016.3");
+		//JOptionPane.showMessageDialog(null, "Olá Mundo!");
 	}
 }
